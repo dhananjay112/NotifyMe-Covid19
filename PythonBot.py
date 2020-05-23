1e0a6ce01a9f3bfff99b9e5dc8453b4be785b5bf
dhananjay112 <35316803+dhananjay112@users.noreply.github.com>
2020-05-23 19:17:52 +0300

    Add files via upload

---
+++ PythonBot.py
@@ -0,0 +1,42 @@
+from plyer import notification
+import requests
+from bs4 import BeautifulSoup
+import time
+
+#for notification
+def notifyMe(title, message):
+    notification.notify(
+        title = title,
+        message = message,
+        app_icon = "C:/Users/dhana/OneDrive - Riga Technical University/Py/Dhinchak/coro.ico",
+        timeout = 4
+    )
+
+#to retrive data from website
+def getData(url):
+    r = requests.get(url)
+    return r.text
+
+if __name__ == "__main__":
+    while True:
+        #notifyMe("KK", "Let's stop the spread of this virus together")
+        myHtmlData = getData("https://www.mohfw.gov.in/")
+
+        soup = BeautifulSoup(myHtmlData, 'html.parser')
+        #print(soup.prettify())
+        myDataStr = ""
+        for tr in soup.find_all('tbody')[0].find_all('tr'):
+            myDataStr += tr.get_text()
+        myDataStr = myDataStr[1:]
+        itemList = myDataStr.split("\n\n")
+
+        states = ['Madhya Pradesh', 'Rajasthan', 'Uttar Pradesh', 'Punjab']
+        for item in itemList[0:34]:
+            dataList = item.split('\n')
+            #print(dataList)
+            if dataList[1] in states:
+                nTitle = 'Cases of Covid-19 in India'
+                nText = f"State {dataList[1]}\nConfirmed :{dataList[2]}\nCured & Migrated : {dataList[3]}\nDeaths : {dataList[4]}"
+                notifyMe(nTitle, nText)
+                time.sleep(2)
+        time.sleep(3600)
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # NotifyMe Covid19
- Personal notification in your computer of number of cases of covid19 cases
+ - Personal notification in your computer of number of cases of covid19 cases
+ - Specifically made for country INDIA, as the cases of COVID-19 are increasing there everyday
+ - specific states can also be selected to get notification for
+ - notification timelapse is changeable
+
+# Product Description
+ - The program creates notification alert on a computer
+ - The program is in python programming languages which is very useful for developing this kind of programs
+ - The notification alert comprise of information regarding COVID-19 pandemic cases in the country 
+ - The selection of this topic give rise to the awareness of the latest information of COVID-19 cases
+ - It renders updated data from official government website of health ministry to your computer
+ - The updated data will be in the form of a push notification, providing cases confirmed and migrated
+
+ # Technologies Used
+ - Python programming language and an IDE to execute the code is used
+ - In python, libraries like plyer, beautiful soap are utilized with requests and time
+ - IDE like Visual studio code can be used to execute the coding
+
+ 
