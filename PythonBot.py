24d4fe32f819ba5ed0cdcf4d35e92f6949713e9b
dhananjay112 <35316803+dhananjay112@users.noreply.github.com>
2020-05-28 19:05:59 +0300

    Add files via upload

--- PythonBot.py
+++ PythonBot.py
@@ -18,25 +18,30 @@ def getData(url):
     return r.text
 
 if __name__ == "__main__":
-    while True:
-        #notifyMe("KK", "Let's stop the spread of this virus together")
-        myHtmlData = getData("https://www.mohfw.gov.in/")
+    try:
+        while True:
+            #notifyMe("KK", "Let's stop the spread of this virus together")
+            myHtmlData = getData("https://www.mohfw.gov.in/")
 
-        soup = BeautifulSoup(myHtmlData, 'html.parser')
-        #print(soup.prettify())
-        myDataStr = ""
-        for tr in soup.find_all('tbody')[0].find_all('tr'):
-            myDataStr += tr.get_text()
-        myDataStr = myDataStr[1:]
-        itemList = myDataStr.split("\n\n")
+            soup = BeautifulSoup(myHtmlData, 'html.parser')
+            #print(soup.prettify())
+            myDataStr = ""
+            for tr in soup.find_all('tbody')[0].find_all('tr'):
+                myDataStr += tr.get_text()
+            myDataStr = myDataStr[1:]
+            itemList = myDataStr.split("\n\n")
 
-        states = ['Madhya Pradesh', 'Rajasthan', 'Uttar Pradesh', 'Punjab']
-        for item in itemList[0:34]:
-            dataList = item.split('\n')
-            #print(dataList)
-            if dataList[1] in states:
-                nTitle = 'Cases of Covid-19 in India'
-                nText = f"State {dataList[1]}\nConfirmed :{dataList[2]}\nCured & Migrated : {dataList[3]}\nDeaths : {dataList[4]}"
-                notifyMe(nTitle, nText)
-                time.sleep(2)
-        time.sleep(3600)
+            states = ['Madhya Pradesh', 'Gujrat', 'Punjab', 'Delhi']
+            for item in itemList[0:34]:
+                dataList = item.split('\n')
+                #print(dataList)
+                if dataList[1] in states:
+                    nTitle = 'Cases of Covid-19 in India'
+                    nText = f"State {dataList[1]}\nConfirmed :{dataList[2]}\nCured & Migrated : {dataList[3]}\nDeaths : {dataList[4]}"
+                    notifyMe(nTitle, nText)
+                    time.sleep(2)
+            time.sleep(20)
+
+    except KeyboardInterrupt:
+        print("Press Ctrl-C to terminate while statement")
+        pass
